d1180d2a99cbdbfbb8055bbd5bc77d3db72a3415
Raphael Sá Brasil <raphael7283@gmail.com>
2024-12-11 21:43:58 -0300

    Configurando urls

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+# HTTP REQUEST <- HTTP RESPONSE
+
+#HTTP REQUEST
+def my_view(request):
+    return HttpResponse('UMA LINDA STRING')
+    # return HTTP Response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
