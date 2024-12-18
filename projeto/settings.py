31b4556809a95c30141b5546fb077167a316af27
Raphael Sá Brasil <raphael7283@gmail.com>
2024-12-17 23:21:48 -0300

    Criando templates global e recipes

--- projeto/settings.py
+++ projeto/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent   #resolve pegar caminho da pasta 'recipes' e da pasta raiz 'curso-django-projeto1'
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render     # tem a função de ler e renderizar arquivo (ex.: template)
 from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
+
+# def home(request):
+#     return HttpResponse('HOME')
+
+# def home(request):
+#     return HttpResponse('''<!DOCTYPE>
+#     <html>
+#     <head><title>Olá Mundo</title></head>
+                        
+#     <body>
+#         <h1>Olá Mundo</h1>
+#     </body>
+#     </html>
+# ''')
 
 def sobre(request):
     return HttpResponse('sobre')
