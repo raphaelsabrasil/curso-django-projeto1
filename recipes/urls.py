8035b29c1f6f38daf05c06e602ee7b894dcdf46f
Raphael Sá Brasil <raphael7283@gmail.com>
2025-01-23 23:04:06 -0300

    Nome único nas URLs e namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 # from recipes.views import home     # , sobre, contato
 from . import views
 
+app_name = 'recipes'        # recipes-home | recipes-recipe
 
 urlpatterns = [    
     # path('', home),
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    # path('', views.home),
+    # path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),      # name="recipes-home"   usando app_name para encurtar name
+    path('recipes/<int:id>', views.recipe, name="recipe"),      # name="recipes-recipe"
 ]
