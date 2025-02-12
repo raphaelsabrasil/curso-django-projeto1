cc0fd104aacb78935854d4bc5fae0b07469896d6
Raphael Sá Brasil <raphael7283@gmail.com>
2025-02-11 22:06:20 -0300

    Usando get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render     # tem a função de ler e renderizar arquivo (ex.: template)
+from django.shortcuts import render, get_list_or_404     # tem a função de ler e renderizar arquivo (ex.: template)
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
-from django.http import Http404
+# from django.http import Http404
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -15,19 +15,26 @@ def home(request):
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id,
+    #     is_published=True,
+    # ).order_by('-id')
 
-    if not recipes:
-        raise Http404('Not found 😢')
+    # if not recipes:
+    #     raise Http404('Not found 😢')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category | ',
+        'title': f'{recipes[0].category.name} - Category | ',
         #'recipes': [make_recipe() for _ in range(10)], status=201,
     })
+    # first() << quando usa queryset // [0] << quando usar uma list
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
