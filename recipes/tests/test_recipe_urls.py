9f566249e0cbf9d61db5626b7e842724f9bb7f46
Raphael Sá Brasil <raphael7283@gmail.com>
2025-02-24 22:46:34 -0300

    aula 76 e 77 package e self.client

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -1,14 +1,11 @@
 from django.test import TestCase    
-from django.urls import reverse, resolve     
+from django.urls import reverse     
 
-from recipes import views
 
 # usa reverse para testar de dentro para fora (ex.: de dentro da view para fora na url desejada)
-
 # no terminal externo >> 'pip install pytest-watch' (deixa automático o teste a cada mudança)
 # usar comando 'ptw'
 
-# Create your tests here.
 class RecipeURLsTest(TestCase): # noqa
     # def test_the_pytest_is_ok(self):
     #    # variavel = '123456'
@@ -30,16 +27,3 @@ class RecipeURLsTest(TestCase): # noqa
 
     # def test_the_pytest_is_ok(self):
     #     assert 1 == 2, 'Um não é igual a dois'
-
-class RecipeViewsTest(TestCase):
-    def test_recipe_home_view_function_is_correct(self):
-        view = resolve(reverse('recipes:home'))
-        self.assertIs(view.func, views.home)
-
-    def test_recipe_category_view_function_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertIs(view.func, views.category)
-
-    def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
