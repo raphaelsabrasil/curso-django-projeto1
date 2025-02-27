705e9fa804f09ee6cd9c55db60c0b06b5f54df33
Raphael Sá Brasil <raphael7283@gmail.com>
2025-02-26 23:13:57 -0300

    Criando fixtures e testando context e content

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
+from recipes import views
+from recipes.models import Category, Recipe, User
 
 # reverse <<< para pegar a url dinamicamente
-
-from recipes import views
 
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
@@ -24,6 +24,39 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here 😢</h1>',
             response.content.decode('utf-8')
         )
+    # Para fazer testes com conteúdo:
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(     # noqa
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        # assert 1 == 1
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
 
 
